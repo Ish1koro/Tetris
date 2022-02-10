615729774b22bdb1b1755e852e9c101a8453f03f
Ishikoro <ytakeda2001@gmail.com>
2022-02-10 13:03:19 +0900

    2/102コマ目終了時

---
+++ Tetris/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+// Playerの操作に関するクラス
+public class PlayerController : MonoBehaviour
+{
+    #region 他スクリプト参照
+    // Playerの入力を取得
+    private PlayerInput _playerinput = default;
+
+    // 配列クラス
+    private Map _map = default;
+
+    // よく使う変数などを定義したクラス
+    private Variables variables = default;
+    #endregion
+
+    #region Vector
+    // Playerの移動の入力値
+    private Vector2 _player_Input_Vector = default;
+    #endregion
+
+    #region int
+    private int _turn_Direction = default;
+    #endregion
+
+    private void Awake()
+    {
+        _playerinput = GetComponent<PlayerInput>();
+        _map = GetComponent<Map>();
+        variables = new Variables();
+    }
+
+    private void OnEnable()
+    {
+        _playerinput.actions[variables._move].performed += Move;
+        _playerinput.actions[variables._turn_Key].started += TurnKey;
+    }
+
+
+    private void OnDisable()
+    {
+        _playerinput.actions[variables._move].performed -= Move;
+        _playerinput.actions[variables._turn_Key].started -= TurnKey;
+    }
+
+    private void Move(InputAction.CallbackContext obj)
+    {
+        _player_Input_Vector = obj.ReadValue<Vector2>();
+        _map.Move(_player_Input_Vector);
+    }
+    private void TurnKey(InputAction.CallbackContext obj)
+    {
+
+    }
+}
---
+++ Tetris/Assets/Scripts/Variables.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 読み取り専用の変数を定義するクラス
+// マジックナンバー防止用
+public class Variables : MonoBehaviour
+{
+    #region int 
+    public int _zero { get; } = 0;
+    public int _one { get; }  = 1;
+    public int _two { get; } = 2;
+    public int _three { get; } = 3;
+    #endregion
+
+    #region float
+    #endregion
+
+    #region InputSystemのAction名
+    public string _move { get; } = "Move";
+    public string _turn_Key { get; } = "TurnKey";
+    public string _turn_Pad { get; } = "TurnPad";
+    #endregion
+}
